b64c16495b6e4923d69de81b8a230be4b1182e2f
garvit <agarvit1142000@gmail.com>
2023-06-16 19:16:09 +0530

    temperorily removed cors

--- Server/src/index.js
+++ Server/src/index.js
@@ -5,36 +5,33 @@ import cookieParser from "cookie-parser";
 
 import connect from "./utils/connect.js";
 
-import userRoutes from "./routes/userRouter.js";
+import { userRouter } from "./routes/userRouter.js";
 const PORT = process.env.PORT || 5000;
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST"],
-  allowedHeaders: ["my-custom-header"],
-  credentials: true,
-};
-
+//const corsOptions = {
+//  origin: "http://localhost:3000",
+//  methods: ["GET", "POST"],
+//  allowedHeaders: ["my-custom-header"],
+//  credentials: true,
+//};
+//
 const app = express(); // Create express app
 
 // Middleware for CORS and Headers
 app.use(cookieParser());
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.disable("x-powered-by");
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
-app.use("/user", userRoutes);
+//app.use(function (req, res, next) {
+//  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//  res.header("Access-Control-Allow-Credentials", true);
+//  next();
+//});
+app.use("/user", userRouter);
 
 // Start server
-server.listen(port, async () => {
-  console.log(server.address);
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log("Server is running on port " + port + " on host " + host);
+app.listen(PORT, async () => {
+  console.log(`Server running on port ${PORT}`);
   await connect();
 });
