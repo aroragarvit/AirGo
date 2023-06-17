0a86dbcf7ea433025fd656b3176f81cc80615e7b
Garvit <agarvit1142000@gmail.com>
2023-06-17 12:48:08 +0530

    made getFlights a post request

--- Server/src/routes/userRouter.js
+++ Server/src/routes/userRouter.js
@@ -13,7 +13,7 @@ import { onboard } from "../controllers/userController/onboard.js";
 userRouter.post("/signup", signup);
 userRouter.get("/verify", verifyEmail);
 userRouter.post("/login", login);
-userRouter.get("/Flights", verifyJwt, getFlights); // make Flights function to get flights ((id, name, seats, and price ) => of particular flight) going from source to destination on a particular date(searching flights by date source and desitnation) (you can use req.body or req.query to get the date, source and destination )
+userRouter.post("/flights", verifyJwt, getFlights); // make Flights function to get flights ((id, name, seats, and price ) => of particular flight) going from source to destination on a particular date(searching flights by date source and desitnation) (you can use req.body or req.query to get the date, source and destination )
 userRouter.post("/book", verifyJwt, bookFlight); // make book function to book a flight (id, name, seats, and price ) => of particular flight going from source to destination on a particular date
 // we can pass user id through req.params  to book flight for that particular user or we can also pass user through verifyJwt middleware and then we can get user id from req.user.id
 userRouter.post("/myBoookings", verifyJwt, bookings);
