62278d5275921533722b6fbaa9352e0952f25cc2
Garvit <agarvit1142000@gmail.com>
2023-06-19 22:27:07 +0530

    Console logginh  flight id

--- Server/src/controllers/adminController/viewBookings.js
+++ Server/src/controllers/adminController/viewBookings.js
@@ -3,15 +3,16 @@ import { User } from "../../models/user.mjs";
 
 export const viewBookings = async (req, res) => {
   const { flightId } = req.body;
+  console.log("Flight id is");
+  console.log(flightId);
+
   if (!flightId) {
     return res.status(400).json({ error: "FlightId is required" });
   }
   try {
     const flight = await Flight.find({
       flightName: flightId,
-    }).populate("bookings.user",
-      "username email -_id"
-    )
+    }).populate("bookings.user", "username email -_id");
     return res.status(200).json({ flight: flight });
   } catch (err) {
     console.error(err);
